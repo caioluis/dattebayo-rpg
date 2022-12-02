9290f0d5e0fb63cc36e1d81d159ebad45c183d52
caioluis <ocaioluis@gmail.com>
2022-12-02 02:44:45 +0000

    feat(tailwind): add breakpoints and plugins
    Signed-off-by: caioluis <ocaioluis@gmail.com>

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,23 @@
 module.exports = {
   content: ["./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    screens: {
+      xs: "400px",
+      s: "520px",
+      sm: "640px",
+      md: "768px",
+      mmd: "860px",
+      lg: "1024px",
+      xl: "1280px",
+      "2xl": "1536px",
+      "3xl": "2560px",
+    },
+    extend: {
+      // extend the bg-neutral colors to include this color: #070606 as neutral-1000
+      colors: {
+        "neutral-1000": "#070606",
+      },
+    },
   },
-  plugins: [],
+  plugins: [require("@tailwindcss/forms")],
 };
