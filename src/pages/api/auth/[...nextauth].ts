34a3e190ab421eea166e57947a899023c861608e
Caio Gomes <ocaioluis@gmail.com>
2022-12-14 03:23:20 +0000

    fix(types): improve typing
    Signed-off-by: Caio Gomes <ocaioluis@gmail.com>

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,3 +1,4 @@
+import type { Session } from "next-auth";
 import NextAuth, { type NextAuthOptions } from "next-auth";
 import DiscordProvider from "next-auth/providers/discord";
 // Prisma adapter for NextAuth, optional and can be removed
@@ -6,6 +7,8 @@ import { PrismaAdapter } from "@next-auth/prisma-adapter";
 import { env } from "../../../env/server.mjs";
 import { prisma } from "../../../server/db/client";
 import type { Prisma } from "@prisma/client";
+import { AdapterUser } from "next-auth/adapters.js";
+import type { JWT } from "next-auth/jwt/types.js";
 
 interface User {
   id: string
@@ -23,18 +26,19 @@ interface User {
 export const authOptions: NextAuthOptions = {
   // Include user.id on session
   callbacks: {
-    session({ session, user }: { session: any, user: User }) {
+    session(params: { session: Session; user: User; token: JWT; }) {
+      const { session, user } = params;
       if (session.user) {
         session.user.id = user.id;
+        session.user.name = user.name;
+        session.user.email = user.email;
+        session.user.emailVerified = user.emailVerified;
+        session.user.image = user.image;
+        session.user.narutomakis = user.narutomakis;
         session.user.maxNumberOfCharacters = user.maxNumberOfCharacters;
         session.user.currentCharacter = user.currentCharacter;
-        session.user.narutomakis = user.narutomakis;
         session.user.birthdate = user.birthdate;
         session.user.createdAt = user.createdAt;
-        session.user.emailVerified = user.emailVerified;
-        session.user.email = user.email;
-        session.user.image = user.image;
-        session.user.name = user.name;
       }
       return session;
     },
