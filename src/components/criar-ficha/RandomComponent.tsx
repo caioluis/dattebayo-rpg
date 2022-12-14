01715ea730200c5ceb68eefc8c7968431c935103
Caio Gomes <ocaioluis@gmail.com>
2022-12-14 02:15:11 +0000

    trigger rebuild
    Signed-off-by: Caio Gomes <ocaioluis@gmail.com>

--- src/components/criar-ficha/RandomComponent.tsx
+++ src/components/criar-ficha/RandomComponent.tsx
@@ -12,7 +12,7 @@ const RandomPage: NextPage = () => {
   
   return (
     <div className="flex flex-col items-center justify-center w-full h-full">
-        <h1 className="text-4xl font-bold">Random Page</h1>
+        <h1 className="text-4xl font-bold">Random Page 1</h1>
     </div>
   );
 };
