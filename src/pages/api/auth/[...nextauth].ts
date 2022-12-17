ab588733169279f741964006e752a2a6d8e978b1
Caio Gomes <ocaioluis@gmail.com>
2022-12-17 22:39:16 +0000

    fix(types): fix changed type
    Signed-off-by: Caio Gomes <ocaioluis@gmail.com>

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,32 +1,32 @@
-import type { Session } from "next-auth";
-import NextAuth, { type NextAuthOptions } from "next-auth";
-import DiscordProvider from "next-auth/providers/discord";
+import type { Session } from 'next-auth';
+import NextAuth, { type NextAuthOptions } from 'next-auth';
+import DiscordProvider from 'next-auth/providers/discord';
 // Prisma adapter for NextAuth, optional and can be removed
-import { PrismaAdapter } from "@next-auth/prisma-adapter";
+import { PrismaAdapter } from '@next-auth/prisma-adapter';
 
-import { env } from "../../../env/server.mjs";
-import { prisma } from "../../../server/db/client";
-import type { Prisma } from "@prisma/client";
-import { AdapterUser } from "next-auth/adapters.js";
-import type { JWT } from "next-auth/jwt/types.js";
+import { env } from '../../../env/server.mjs';
+import { prisma } from '../../../server/db/client';
+import type { Prisma } from '@prisma/client';
+import { AdapterUser } from 'next-auth/adapters.js';
+import type { JWT } from 'next-auth/jwt/types.js';
 
 interface User {
-  id: string
-  name: string | null
-  email: string | null
-  emailVerified: Date | null
-  image: string | null
-  narutomakis: Prisma.Decimal | null
-  maxNumberOfCharacters: number | null
-  currentCharacter: number | null
-  birthdate: Date | null
-  createdAt: Date
+  id: string;
+  name: string | null;
+  email: string | null;
+  emailVerified: Date | null;
+  image: string | null;
+  narutomakis: Prisma.Decimal | null;
+  maxNumberOfCharacters: number;
+  currentCharacter: number | null;
+  birthdate: Date | null;
+  createdAt: Date;
 }
 
 export const authOptions: NextAuthOptions = {
   // Include user.id on session
   callbacks: {
-    session(params: { session: Session; user: User; token: JWT; }) {
+    session(params: { session: Session; user: User; token: JWT }) {
       const { session, user } = params;
       if (session.user) {
         session.user.id = user.id;
@@ -41,16 +41,22 @@ export const authOptions: NextAuthOptions = {
         session.user.createdAt = user.createdAt;
       }
       return session;
-    },
+    }
   },
   // Configure one or more authentication providers
   adapter: PrismaAdapter(prisma),
   providers: [
     DiscordProvider({
-      clientId: env.NODE_ENV === 'production' ? env.DISCORD_CLIENT_ID : env.DEV_DISCORD_CLIENT_ID,
-      clientSecret: env.NODE_ENV === 'production' ? env.DISCORD_CLIENT_SECRET : env.DEV_DISCORD_CLIENT_SECRET,
-    }),
-  ],
+      clientId:
+        env.NODE_ENV === 'production'
+          ? env.DISCORD_CLIENT_ID
+          : env.DEV_DISCORD_CLIENT_ID,
+      clientSecret:
+        env.NODE_ENV === 'production'
+          ? env.DISCORD_CLIENT_SECRET
+          : env.DEV_DISCORD_CLIENT_SECRET
+    })
+  ]
 };
 
 export default NextAuth(authOptions);
