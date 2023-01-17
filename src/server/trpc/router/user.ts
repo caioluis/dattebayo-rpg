470deaaeb7005dc875827650cf7f6022a176a19e
Caio Gomes <ocaioluis@gmail.com>
2023-01-17 01:00:24 +0000

    feat(userRouter): create editName procedure

--- src/server/trpc/router/user.ts
+++ src/server/trpc/router/user.ts
@@ -1,21 +1,55 @@
-import { z } from "zod";
+import { z } from 'zod';
 
-import { router, protectedProcedure } from "../trpc";
+import { router, protectedProcedure } from '../trpc';
 
 export const userRouter = router({
-    setCurrentCharacter: protectedProcedure
-        .input(z.object({
-            id: z.string(),
-            currentCharacterId: z.number(),
-        }))
-        .mutation(({ input, ctx }) => {
-            return ctx.prisma.user.update({
-                where: {
-                    id: input.id,
-                },
-                data: {
-                    currentCharacter: input.currentCharacterId,
-                }
-            })
-        })
+  editName: protectedProcedure
+    .input(
+      z.object({
+        id: z.string(),
+        currentName: z.string(),
+        newName: z.string()
+      })
+    )
+    .mutation(async ({ input, ctx }) => {
+      if (input.currentName === input.newName) {
+        throw new Error('O nome atual é o mesmo que o novo nome.');
+      }
+
+      const isThereAnUserWithNewName = await ctx.prisma.user.findUnique({
+        where: {
+          name: input.newName
+        }
+      });
+
+      if (isThereAnUserWithNewName !== null) {
+        throw new Error('Já existe um usuário com esse nome.');
+      }
+
+      return ctx.prisma.user.update({
+        where: {
+          id: input.id
+        },
+        data: {
+          name: input.newName
+        }
+      });
+    }),
+  setCurrentCharacter: protectedProcedure
+    .input(
+      z.object({
+        id: z.string(),
+        currentCharacterId: z.number()
+      })
+    )
+    .mutation(({ input, ctx }) => {
+      return ctx.prisma.user.update({
+        where: {
+          id: input.id
+        },
+        data: {
+          currentCharacter: input.currentCharacterId
+        }
+      });
+    })
 });
