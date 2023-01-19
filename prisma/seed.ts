7132303aa2d1b4e278bde87016313907db786d0f
Caio Gomes <ocaioluis@gmail.com>
2023-01-19 02:42:25 +0000

    test(user): update db seed for tests and deleted conflicting migration

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,20 +1,23 @@
 import { PrismaClient } from '@prisma/client';
+
 const prisma = new PrismaClient();
 async function main() {
   const Akihito = await prisma.user.upsert({
     where: { email: 'akihito@dattebayo.ninja' },
     update: {},
     create: {
+      id: 'cld2aoyco00000hl8u74e33m0',
       name: 'Akihito',
+      nameWasLastChangedAt: '2022-01-17T15:25:31.476Z',
       email: 'akihito@dattebayo.ninja',
-      emailVerified: new Date('2022-01-18T012:30:00.000Z'),
+      emailVerified: null,
       image:
         'https://cdn.discordapp.com/avatars/466564113531666442/2c27cce69f6616c2588de11ac46aa5f3.webp?size=100',
       birthdate: null,
       narutomakis: 0,
       currentCharacter: null,
       maxNumberOfCharacters: 1,
-      createdAt: new Date('2022-01-18T012:15:00.000Z')
+      createdAt: '2023-01-18T19:25:31.476Z'
     }
   });
   console.log({ Akihito });
@@ -28,3 +31,5 @@ main()
     await prisma.$disconnect();
     process.exit(1);
   });
+
+export {};
