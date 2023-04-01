c52ecbffacb43e91c7b992b725a5a6eb775b3865
Caio Gomes <ocaioluis@gmail.com>
2023-04-01 01:08:20 +0100

    fix(index): disable non-null assertion
    Signed-off-by: Caio Gomes <ocaioluis@gmail.com>

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,7 @@
+// I turned on the eslint-disable because Typescript does not know that inside SignedIn is impossible
+// for user to be undefined, but I don't know how to tell Typescript that.
+
+/* eslint-disable @typescript-eslint/no-non-null-assertion */
 import Image from "next/image";
 import { type NextPage } from "next";
 import { RedirectToSignIn, SignedIn, SignedOut, useUser } from "@clerk/nextjs";
@@ -43,7 +47,7 @@ const Home: NextPage = () => {
           </div>
           <Container>
             <NarrowContainer>
-              <>{!userHasCharacter && <CallToCreateCharacter userId={user?.id} />}</>
+              <>{!userHasCharacter && <CallToCreateCharacter userId={user!.id} />}</>
               <Mural />
               <RegrasETutoriais />
             </NarrowContainer>
