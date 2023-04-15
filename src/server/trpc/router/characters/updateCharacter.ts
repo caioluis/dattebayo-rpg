1338e9f451b64780c884ffc6ae9498d9e8edce8b
Caio Gomes <ocaioluis@gmail.com>
2023-04-15 22:30:08 +0100

    feat(schema): update character schema to second phase
    Signed-off-by: Caio Gomes <ocaioluis@gmail.com>

--- src/server/trpc/router/characters/updateCharacter.ts
+++ src/server/trpc/router/characters/updateCharacter.ts
@@ -6,21 +6,51 @@ export const updateCharacter = {
     .input(
       z.object({
         id: z.number(),
-        name: z.string().optional(),
-        avatar: z.string().optional(),
-        icon: z.string().optional(),
-        villageId: z.number().optional(),
-        clanId: z.number().optional(),
-        rank: z.string().optional(),
-        kekkeiGenkai: z.string().optional(),
         age: z.number().optional(),
-        height: z.number().optional(),
+        name: z.string().optional(),
+        icon: z.string().optional(),
+        story: z.string().optional(),
+        avatar: z.string().optional(),
+        clanId: z.number().optional(),
+        rankId: z.number().optional(),
         weight: z.number().optional(),
-        personality: z.string().optional(),
+        height: z.number().optional(),
+        villageId: z.number().optional(),
         appearance: z.string().optional(),
+        personality: z.string().optional(),
         photoplayer: z.string().optional(),
+        kekkeiGenkai: z.string().optional(),
         backgroundStory: z.string().optional(),
-        story: z.string().optional()
+
+        ninjutsu: z.number().optional(),
+        genjutsu: z.number().optional(),
+        taijutsu: z.number().optional(),
+        stamina: z.number().optional(),
+        intelligence: z.number().optional(),
+        strength: z.number().optional(),
+        speed: z.number().optional(),
+        handSeals: z.number().optional(),
+
+        yin: z.number().optional(),
+        yang: z.number().optional(),
+        bukijutsu: z.number().optional(),
+        combat: z.number().optional(),
+        shurikenjutsu: z.number().optional(),
+        fuinjutsu: z.number().optional(),
+        raiton: z.number().optional(),
+        futon: z.number().optional(),
+        doton: z.number().optional(),
+        suiton: z.number().optional(),
+        katon: z.number().optional(),
+        kekkaijutsu: z.number().optional(),
+        kyuinjutsu: z.number().optional(),
+        nagashi: z.number().optional(),
+        kanchi: z.number().optional(),
+
+        hp: z.number().optional(),
+        chakra: z.number().optional(),
+
+        luck: z.number().optional()
       })
     )
     .mutation(async ({ input, ctx }) => {
@@ -29,21 +59,51 @@ export const updateCharacter = {
           id: input.id
         },
         data: {
-          name: input.name || undefined,
-          avatar: input.avatar || undefined,
-          icon: input.icon || undefined,
-          villageId: input.villageId || undefined,
-          clanId: input.clanId || undefined,
-          rank: input.rank || undefined,
-          kekkeiGenkai: input.kekkeiGenkai || undefined,
           age: input.age || undefined,
-          height: input.height || undefined,
+          name: input.name || undefined,
+          icon: input.icon || undefined,
+          story: input.story || undefined,
+          avatar: input.avatar || undefined,
+          clanId: input.clanId || undefined,
+          rankId: input.rankId || undefined,
           weight: input.weight || undefined,
-          personality: input.personality || undefined,
+          height: input.height || undefined,
+          villageId: input.villageId || undefined,
           appearance: input.appearance || undefined,
+          personality: input.personality || undefined,
           photoplayer: input.photoplayer || undefined,
+          kekkeiGenkai: input.kekkeiGenkai || undefined,
           backgroundStory: input.backgroundStory || undefined,
-          story: input.story || undefined
+
+          ninjutsu: input.ninjutsu || undefined,
+          genjutsu: input.genjutsu || undefined,
+          taijutsu: input.taijutsu || undefined,
+          stamina: input.stamina || undefined,
+          intelligence: input.intelligence || undefined,
+          strength: input.strength || undefined,
+          speed: input.speed || undefined,
+          handSeals: input.handSeals || undefined,
+
+          yin: input.yin || undefined,
+          yang: input.yang || undefined,
+          bukijutsu: input.bukijutsu || undefined,
+          combat: input.combat || undefined,
+          shurikenjutsu: input.shurikenjutsu || undefined,
+          fuinjutsu: input.fuinjutsu || undefined,
+          raiton: input.raiton || undefined,
+          futon: input.futon || undefined,
+          doton: input.doton || undefined,
+          suiton: input.suiton || undefined,
+          katon: input.katon || undefined,
+          kekkaijutsu: input.kekkaijutsu || undefined,
+          kyuinjutsu: input.kyuinjutsu || undefined,
+          nagashi: input.nagashi || undefined,
+          kanchi: input.kanchi || undefined,
+
+          hp: input.hp || undefined,
+          chakra: input.chakra || undefined,
+
+          luck: input.luck || undefined
         }
       });
     })
